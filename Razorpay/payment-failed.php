7db30a898a2fd2a4baa5529c1e3c26b450c3853f
AsimJib <35167938+AsimJib@users.noreply.github.com>
2025-03-22 22:32:27 +0530

    Delete Razorpay directory

--- Razorpay/payment-failed.php
+++
@@ -1,4 +0,0 @@
-<?php 
-echo "<h1>Payment Failed</h1>";
-echo "<p>Unfortunately, your payment could not be processed. Please try again or contact support if the issue persists.</p>";
-?>
--- Razorpay/payment-success.php
+++
@@ -1,4 +0,0 @@
-<?php 
-echo "<h1>Payment Successful!</h1>";
-echo "<p>Your payment was completed successfully. Thank you for your payment!</p>";
-?>
